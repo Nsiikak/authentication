5f2380b46077d62c58d7b2dc41c9d0e584ed0978
Nsiikak <nsikakebong98@gmail.com>
2024-08-09 14:09:06 +0100

    "Updated dependencies and configuration in package.json and app.module.ts"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule globally available
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
